79718cfaabd223b941e40e7f02c98f590ee69d3f
Alejandro Rodriguez <aleronal@hotmail.com>
2023-08-12 13:29:59 +0100

    Initial Commit

---
+++ SwiftuiMap/SwiftuiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapApp.swift
+//  SwiftuiMap
+//
+//  Created by Alejandro Rodriguez on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
